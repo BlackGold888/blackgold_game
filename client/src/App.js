791f4e4e637e7468d7be8ed2d86feef0c1617c48
blackgold888 <khamidalakkhali@gmail.com>
2022-08-15 18:58:40 +0600

    -- Add MUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 import {emitter as Emitter} from './events';
 import { GameSocket } from './socket';
 import { useEffect, useState } from 'react';
+import { Box, Button, Container, Paper, Stack, styled } from '@mui/material';
+import * as PropTypes from 'prop-types';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
 
 function App() {
     const [socket, setSocket] = useState(new GameSocket('ws://localhost:3000'));
@@ -24,14 +33,21 @@ function App() {
     }, []);
 
     return (
-        <div className="App">
-            <label htmlFor="">Name</label>
-            <input type="text"/>
-            <button onClick={ () => {
-                addClient();
-            } }>Click
-            </button>
-        </div>
+        <Container maxWidth="sm" sx={
+            {
+                justifyContent: 'center',
+                alignItems: 'center',
+                display: 'flex',
+            }
+        }>
+            <Box sx={{ width: '100%' }}>
+                <Stack spacing={4}>
+                    <Item>Item 1</Item>
+                    <Item>Item 2</Item>
+                    <Item>Item 3</Item>
+                </Stack>
+            </Box>
+        </Container>
     );
 }
 
